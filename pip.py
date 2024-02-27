c57b7dcf204ab7e81a34d7c56bd902e29222a73a
Glory_Kuria <kuriaglory@gmail.com>
2024-02-27 13:26:20 +0300

    add files

--- pip.py
+++ pip.py
@@ -1,4 +1,31 @@
+print("select option")
+print("1 MULTIPLY")
+print("2 Division")
+print("3 SUBTRACT")
+print("4 ADD")
 
+
+
+
+
+
+operation = input()
+if operation == "1":
+    num1= input ("enter first number:")
+    num2= input ("enter second number:")
+    print("the multiply is:" + str(int(num1) * int(num2)))
+elif operation == "2":
+    num1= input("enter first number:")
+    num2= input("enter second number:")
+    print("the Division is:" + str(int(num1) / int(num2)))
+elif operation == "3":
+    num1= input("enter first number:")
+    num2= input("enter second number:")
+    print("the subtract is:" + str(int(num1) - int(num2)))
+elif operation == "4":
+    num1= input("enter first number:")
+    num2= input("enter second number:")
+    print("the sum is;" + str(int(num1) + int(num2)))
 
 
 
